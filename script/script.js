5d36e8084056995f5af83e7734f9b092c2b2da5e
Andrew Shuvaev <andrew.shuw@gmail.com>
2023-11-20 17:51:21 +1000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch and 'origin/main' have diverged,
    and have 1 and 3 different commits each, respectively.
    
    Changes to be committed:
    	new file:   .gitignore
    	modified:   css/style.css
    	new file:   fonts/AvenirNext-Heavy.ttf
    	new file:   fonts/AvenirNext-Heavy.woff
    	new file:   fonts/AvenirNext-Heavy.woff2
    	new file:   fonts/stylesheet.css
    	new file:   gulpfile.js
    	modified:   img/CA.svg
    	modified:   img/CS.svg
    	new file:   img/Group 5.svg
    	modified:   img/RS.svg
    	new file:   img/back-icon.png
    	new file:   img/carousel_1.svg
    	new file:   img/carousel_2.svg
    	new file:   img/carousel_3.svg
    	new file:   img/carousel_4.svg
    	new file:   img/carousel_5.svg
    	new file:   img/porche.svg
    	new file:   img/slide-1.svg
    	new file:   img/slide-2.svg
    	new file:   img/slide-3.svg
    	new file:   img/slide-4.png
    	new file:   img/slide-5.png
    	new file:   img/slide-6.png
    	new file:   img/slide-7.png
    	modified:   index.html
    	new file:   npm-shrinkwrap.json
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   script/script.js
    	new file:   scss/custom.scss
    	new file:   src/assets/js/app.js
    	new file:   src/assets/js/file2.js
    	new file:   src/assets/scss/style.scss
    	new file:   src/index.html
    	new file:   src/layouts/default.html
    	new file:   src/partials/footer.html
    	new file:   src/partials/head.html
    	new file:   src/partials/header.html
    	new file:   test2.html
    
    Changes not staged for commit:
    	modified:   script/script.js

---
+++ script/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        dots: false,
+        slidesToShow: 3, 
+        slidesToScroll: 1, 
+        prevArrow: '<button type="button" class="slick-prev">&#9665;</button>',
+        nextArrow: '<button type="button" class="slick-next">&#9655;</button>',
+        responsive: [
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
